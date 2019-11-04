6bd97d3412c20abebed7162da7d0e3345b0bac63
roedoejet <aidanpine@shaw.ca>
2019-11-04 12:18:35 -0800

    remove unnecessary sphinx package

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,6 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx_click.ext'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
